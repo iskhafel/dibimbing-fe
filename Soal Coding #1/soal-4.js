43310eb0f01923e5456f6c17827d9f31ffa3e79b
faishalkamali <111752556+faishalkamali@users.noreply.github.com>
2024-08-23 20:26:37 +0700

    huge update

--- Soal Coding #1/soal-4.js
+++ Soal Coding #1/soal-4.js
@@ -12,7 +12,7 @@
 const prompt = require("prompt-sync")();
 
 let x = 0;
-const inputs = []
+const inputs = [];
 
 while (true) {
   const input = prompt("input ? ");
@@ -20,11 +20,10 @@ while (true) {
   if (input.match(/^\d+$/)) {
     const number = Number(input);
     x += number;
-    inputs.push(number)
+    inputs.push(number);
   } else {
     break;
   }
 }
 
-
-console.log(`jumlah = ${inputs.join(' + ')} = ${x}`); // ini diganti dengan jumlah seluruh inputnya
+console.log(`jumlah = ${inputs.join(" + ")} = ${x}`); // ini diganti dengan jumlah seluruh inputnya
